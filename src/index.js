0ad050e1ed51f8f854ed2ffe3e6c1a8d24b7088d
fereshteh <fereshteh.kalantar@gmail.com>
2023-02-04 10:52:41 +0330

    updated time and date

--- src/index.js
+++ src/index.js
@@ -6,6 +6,50 @@ console.log(url);
 
 //axios.get(url).then(showTemprature);
 
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let day = days[date.getDay()];
+  let currentMonths = date.getMonth();
+  let currentDate = date.getDate();
+  let timeElement = document.querySelector("#time");
+  timeElement.innerHTML = `${hours}:${minutes}`;
+  return `${day}, ${months[currentMonths]} ${currentDate}`;
+}
+
 function showTemperature(response) {
   console.log(response);
   let cityElement = document.querySelector("h1");
@@ -13,6 +57,7 @@ function showTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   //let iconElement=document.querySelector("#icon")
 
   cityElement.innerHTML = response.data.name;
@@ -20,6 +65,7 @@ function showTemperature(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
   windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   //iconElement.querySelector=response.
 }
 
